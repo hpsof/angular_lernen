d72b52acb90ffad6b5f42ef6a1947d2bda71488b
hpsof <hanspeter.schlapbach@sofim.com>
2014-08-29 10:30:21 +0200

    initial

---
+++ app.js
@@ -0,0 +1,12 @@
+var app = angular.module("app", []);
+
+app.controller("AppCtrl", function () {
+    var app = this;
+    app.message= "guet isch abig mit bootstrap";
+});
+
+app.directive("hpsof", function () {
+    return {
+
+    }
+});
